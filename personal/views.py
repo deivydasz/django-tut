fe1253fcd1b2872d4cda4aa955c643c5cd61b0bc
ecuSinner <deivydas.ziziliauskas@gmail.com>
2017-01-23 15:56:41 +0200

    Added contact page

--- personal/views.py
+++ personal/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def index(request):
 	return render(request, 'personal/home.html')
 # Create your views here.
+
+def contact(request):
+	return render(request, 'personal/basic.html', {
+		'contacts':[
+		'If you would like to contact me, please email me',
+		'test@gmail.com',
+		]
+		})
